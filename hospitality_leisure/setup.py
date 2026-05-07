efb2959e7caf12ae61d3343ce77021e7780de72c
Balaji K <balajik@Balajis-MacBook-Air.local>
2026-05-07 17:53:56 +0530

    Fix: Simplified setup.py to resolve bench get-app FileNotFoundError

--- hospitality_leisure/setup.py
+++ hospitality_leisure/setup.py
@@ -1,19 +1,16 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
-
-# get version from __version__ variable in hospitality_leisure/__init__.py
-from hospitality_leisure import __version__ as version
-
 setup(
 	name="hospitality_leisure",
-	version=version,
+	version="1.0.0",
 	description="Complete Hospitality and Leisure ERP Solution",
 	author="Your Organization",
 	author_email="support@yourorg.com",
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires
+	install_requires=[
+		"frappe",
+		"erpnext"
+	]
 )
